102a73391939fe92f93ba95a2bc9fd80d2d42471
Rishavrishu-creator <66913014+Rishavrishu-creator@users.noreply.github.com>
2020-10-11 13:39:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var crypto = require('crypto')
-
+var aes256 = require('aes256')
 var alice = crypto.createECDH('secp256k1')
 alice.generateKeys()
 var bob = crypto.createECDH('secp256k1')
@@ -13,3 +13,7 @@ var bobShraedKey  = bob.computeSecret(alicePublicKeyBase64,'base64','hex')
 
 console.log(aliceSharedKey==bobShraedKey)
 console.log(aliceSharedKey)
+var message = "Some random message"
+var encrypted = aes256.encrypt(aliceSharedKey,message)
+var decrypted = aes256.decrypt(bobShraedKey,encrypted)
+console.log(decrypted)
