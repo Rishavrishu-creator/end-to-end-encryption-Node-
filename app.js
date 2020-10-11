b87e328bc79b4fae9ed602fc6f3bd5907575dc5d
Rishavrishu-creator <66913014+Rishavrishu-creator@users.noreply.github.com>
2020-10-11 13:33:07 +0530

    commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var crypto = require('crypto')
 
-var alice =crypto.getDiffieHellman('modp15')
-var bob = crypto.getDiffieHellman('modp15')
-
+var alice = crypto.createECDH('secp256k1')
 alice.generateKeys()
+var bob = crypto.createECDH('secp256k1')
 bob.generateKeys()
-var aliceSecret = alice.computeSecret(bob.getPublicKey(),null,'hex')
-var bobSecret = bob.computeSecret(alice.getPublicKey(),null,'hex')
 
-console.log(aliceSecret == bobSecret)
-console.log(aliceSecret)
+var alicePublicKeyBase64 = alice.getPublicKey().toString('base64')
+var bobPublicKeyBase64 = bob.getPublicKey().toString('base64')
 
+var aliceSharedKey = alice.computeSecret(bobPublicKeyBase64,'base64','hex')
+var bobShraedKey  = bob.computeSecret(alicePublicKeyBase64,'base64','hex')
+
+console.log(aliceSharedKey==bobShraedKey)
+console.log(aliceSharedKey)
